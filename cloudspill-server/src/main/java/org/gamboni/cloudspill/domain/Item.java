d8c59b4d87c3d0592b8806725121d519b79e49b7
Maxime Gamboni <tendays@gamboni.org>
2020-02-16 18:54:09 +0100

    Add missing @Transient

--- cloudspill-server/src/main/java/org/gamboni/cloudspill/domain/Item.java
+++ cloudspill-server/src/main/java/org/gamboni/cloudspill/domain/Item.java
@@ -12,6 +12,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 import javax.persistence.Version;
 
 import org.gamboni.cloudspill.shared.domain.JpaItem;
@@ -37,6 +38,7 @@ public class Item extends JpaItem {
 		return id;
 	}
 
+	@Transient
 	public Long getServerId() {
 		return id;
 	}
