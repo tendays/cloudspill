cbd22ee759b845ff1968e6c991a30b63828b7096
Maxime Gamboni <tendays@gamboni.org>
2020-04-17 17:27:13 +0200

    Missing @Id annotation

--- cloudspill-server/src/main/java/org/gamboni/cloudspill/domain/Item.java
+++ cloudspill-server/src/main/java/org/gamboni/cloudspill/domain/Item.java
@@ -6,6 +6,7 @@ package org.gamboni.cloudspill.domain;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  * An Item represents a file. Hierarchy is as follows: /user/folder/path where
@@ -15,7 +16,7 @@ import javax.persistence.GenerationType;
  */
 @Entity
 public class Item extends BackendItem {
-	
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public long getId() {
 		return this.id;
